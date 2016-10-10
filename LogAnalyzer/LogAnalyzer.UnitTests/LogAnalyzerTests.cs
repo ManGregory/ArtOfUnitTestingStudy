98ce8c1963dcb96e19e2260bd9a8648e6e84af72
ManGregory <manaenkov.gregory@gmail.com>
2016-10-10 21:28:24 +0300

    Added LogAnalyzer

---
+++ LogAnalyzer/LogAnalyzer.UnitTests/LogAnalyzerTests.cs
@@ -0,0 +1,46 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogAnalyzer.UnitTests
+{
+    [TestFixture]
+    public class LogAnalyzerTests
+    {
+        LogAnalyzer logAnalyzer;
+
+        [SetUp]
+        public void Setup()
+        {
+            logAnalyzer = new LogAnalyzer();
+        }
+
+        [Test]
+        public void IsValidLogFileName_BadExtension_ReturnsFalse()
+        {
+            bool result = logAnalyzer.IsValidLogFileName("badextension.yzf");
+
+            Assert.False(result);
+        }
+
+        [TestCase("goodext.slf")]
+        [TestCase("goodext.SLF")]
+        public void IsValidLogFileName_ValidExtension_ReturnsTrue(string fileName)
+        {          
+            bool result = logAnalyzer.IsValidLogFileName(fileName);
+
+            Assert.True(result);
+        }
+
+        [Test]
+        public void IsValidLogFileName_EmptyFileName_Throws()
+        {
+            var ex = Assert.Catch<Exception>(() => logAnalyzer.IsValidLogFileName(string.Empty));
+
+            StringAssert.Contains("file name should be set", ex.Message);
+        }
+    }
+}
---
+++ LogAnalyzer/LogAnalyzer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogAnalyzer
+{
+    public class LogAnalyzer
+    {
+        public bool IsValidLogFileName(string fileName)
+        {
+            if (string.IsNullOrEmpty(fileName))
+            {
+                throw new ArgumentException("file name should be set");
+            }
+            return fileName.EndsWith(".SLF", StringComparison.CurrentCultureIgnoreCase);
+        }
+    }
+}
